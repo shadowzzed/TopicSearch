301f71d0ce7d7a37c7d8ff8e182fee0723910646
Zed <shadowl91@163.com>
2020-04-29 15:48:56 +0800

    add mybatis tkmybatis

--- data-in/src/main/java/com/zed/topic/search/data/in/DataInApplication.java
+++ data-in/src/main/java/com/zed/topic/search/data/in/DataInApplication.java
@@ -5,10 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScans;
+import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
 @ComponentScan({"com.zed.topic.search.core","com.zed.topic.search.data.in"})
 @EnableDiscoveryClient
+@MapperScan(basePackages = "com.zed.topic.search.core.mapper")
 public class DataInApplication {
 
     public static void main(String[] args) {
